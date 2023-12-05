663ee7a6f171494daafb38a7b27b010908cf345d
reilandeubank <113558472+reilandeubank@users.noreply.github.com>
2023-12-05 14:18:31 -0600

    Remove erroneous function

--- pkg/scanner/token.go
+++ pkg/scanner/token.go
@@ -26,7 +26,3 @@ func NewToken(tokenType TokenType, lexeme string, literal interface{}, line int)
 func (t Token) String() string {
 	return fmt.Sprintf("%d %s %v", t.Type, t.Lexeme, t.Literal)
 }
-
-func Tester() string {
-	return "hello"
-}
