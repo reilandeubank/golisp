19e281664d09028380098a64e3aecf8805909425
reilandeubank <reiland.eubank@yahoo.com>
2024-01-01 17:04:19 -0600

    add support for 'exit' command

--- pkg/interpreter/environment.go
+++ pkg/interpreter/environment.go
@@ -5,27 +5,27 @@ import (
 	"os"
 )
 
-// environment allows for variable scope and closures. Less necessary in this implementation of Lisp
-type environment struct {
-	enclosing *environment
+// Environment allows for variable scope and closures. Less necessary in this implementation of Lisp
+type Environment struct {
+	enclosing *Environment
 	values    map[string]interface{}
 }
 
-func NewEnvironment() environment {
-	return environment{enclosing: nil, values: make(map[string]interface{})}
+func NewEnvironment() Environment {
+	return Environment{enclosing: nil, values: make(map[string]interface{})}
 }
 
-func NewEnvironmentWithEnclosing(Enclosing environment) environment {
-	return environment{enclosing: &Enclosing, values: make(map[string]interface{})}
+func NewEnvironmentWithEnclosing(Enclosing Environment) Environment {
+	return Environment{enclosing: &Enclosing, values: make(map[string]interface{})}
 }
 
 // define a variable name as the passed value. only allowed in global scope
-func (e *environment) define(name string, value interface{}) {
+func (e *Environment) define(name string, value interface{}) {
 	e.values[name] = value // this allows for variable redefinition. May be weird in normal code, but is useful for REPL
 }
 
 // get the value of a variable name. searches in enclosing environments, but throws if the variable is never found
-func (e *environment) get(name scanner.Token) (interface{}, error) {
+func (e *Environment) get(name scanner.Token) (interface{}, error) {
 	value, ok := e.values[name.Lexeme]
 	if !ok && e.enclosing != nil {
 		return e.enclosing.get(name)
